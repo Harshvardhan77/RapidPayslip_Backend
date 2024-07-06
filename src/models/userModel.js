fdf5b4e23e51d5be1d0943c8d6de677ce5343d31
Harshvardhan77 <harshvardhan2024@ipsacademy.org>
2024-07-06 13:14:10 +0530

    bcrypt changes to bycryptjs

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 const userSchema= mongoose.Schema(
     {
